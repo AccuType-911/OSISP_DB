712f56e53e596083ef6c775a155ac7b6b87f4af0
AccuType-911 <profprof1997@gmail.com>
2015-12-12 18:41:37 +0300

    StartParserDevelopment
    Done normal interfaces for parser. Iterator was added inside the parser

--- OSiSP_DB/Parser.cpp
+++ OSiSP_DB/Parser.cpp
@@ -8,7 +8,41 @@ Parser::Parser(wstring path)
 	this->file_path = path;
 }
 
-
 Parser::~Parser()
 {
+
 }
+
+
+Parser::iterator Parser::get_iterator()
+{
+	HANDLE hMapFile;
+	wchar_t *pBuf;
+
+	hMapFile = CreateFileMappingW(
+		INVALID_HANDLE_VALUE,    // use paging file
+		NULL,                    // default security
+		PAGE_READWRITE,          // read/write access
+		0,                       // maximum object size (high-order DWORD)
+		BUFFER_SIZE,                // maximum object size (low-order DWORD)
+		this->file_path.c_str());                 // name of mapping object
+
+	if (hMapFile == NULL)
+	{
+		_tprintf(TEXT("Could not create file mapping object (%d).\n"),
+			GetLastError());
+		return;
+	}
+
+	Parser::iterator it(hMapFile);
+	return it;
+
+	//CopyMemory((PVOID)pBuf, szMsg, (_tcslen(szMsg) * sizeof(TCHAR)));
+	//_getch();
+
+//	UnmapViewOfFile(pBuf);
+//	CloseHandle(hMapFile);
+}
+
+//iterator<Human> get_iterator(); 	5 laba
+long get_db_size() { return -1; }
--- OSiSP_DB/Parser.h
+++ OSiSP_DB/Parser.h
@@ -3,19 +3,102 @@
 #include <vector>
 #include <string>
 
+#include <windows.h>
+#include <stdio.h>
+#include <conio.h>
+#include <tchar.h>
+
 class Parser
 {
-	vector<Human> fetchHumansObjects_FromFile();
-                	
-	long get_db_size();			
-	//iterator<Human> get_iterator(); 	
-	long get_db_size();			// גמחנאשאוע נאחלונ פאיכא
+	#define BUFFER_SIZE 1024
 
 public:
+	class iterator;
+
+                	
+	long get_db_size();			
+	iterator get_iterator(); 	
+	long get_db_size();			// גמחנאשאוע נאחלונ פאיכא
 	Parser(wstring path);
 	~Parser();
+
 	
+	vector<Human> fetchHumansObjects_FromFile() {
+		
+	}
+
+
 private:
 	wstring file_path;
+
+
+
+
+
+
+
+
+
+
+
+public:
+	class iterator
+	{
+	public:
+		typedef iterator self_type;
+		typedef Human value_type;
+		typedef Human& reference;
+		typedef Human* pointer;
+		typedef std::forward_iterator_tag iterator_category;
+		typedef int difference_type;
+
+		iterator(HANDLE hMapFile) {
+			//this->ptr = ptr;
+			this->hMapFile = hMapFile;
+			this->ptr = (wchar_t*)MapViewOfFile(hMapFile,   // handle to map object
+				FILE_MAP_ALL_ACCESS, // read/write permission
+				0,
+				0,
+				BUFFER_SIZE);
+
+			if (this->ptr == NULL)
+			{
+				_tprintf(TEXT("Could not map view of file (%d).\n"),
+					GetLastError());
+
+				CloseHandle(hMapFile);
+
+				return;
+			}
+		}
+
+		self_type operator++() {
+			self_type i = *this;
+
+			while (has_next()) 
+			{
+				
+			}
+			
+			return i;
+		}
+		pointer operator->() {
+			return ptr_;
+		}
+		
+
+		bool has_next();
+	private:
+		pointer ptr_;
+		WCHAR *ptr;
+		HANDLE hMapFile;
+		bool is_EOF;
+
+		Human* getHuman() 
+		{
+			
+		}
+	};
+
 };
 
